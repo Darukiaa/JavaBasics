179d1ac0843437496a1539267a544f296d24fe56
Marcel Hodan <marcel.hodan@mahosoftware.de>
2021-01-03 10:23:37 +0100

    used CTRL+F in eclipse to format your code

--- Uebung09/src/main/java/uebung09/ClockTime.java
+++ Uebung09/src/main/java/uebung09/ClockTime.java
@@ -15,197 +15,174 @@ public class ClockTime {
 	public ClockTime(int h) {
 		if (h < 0) {
 			this.h = 24 + h;
-		}
-			else {
-				if (h >=24) {
-					this.h = 0 + (h - 24);
-				}
-				else {
-			this.h = h;
-				}
+		} else {
+			if (h >= 24) {
+				this.h = 0 + h - 24;
+			} else {
+				this.h = h;
+			}
 		}
 		m = 0;
 		s = 0;
-		
+
 	}
 
 	public ClockTime(int h, int m) {
 		if (h < 0) {
 			this.h = 24 + h;
+		} else {
+			if (h >= 24) {
+				this.h = 0 + h - 24;
+			} else {
+				this.h = h;
 			}
-			else {
-				if (h >=24) {
-					this.h = 0 + (h - 24);
+			if (m < 0) {
+				this.m = 60 + m;
+				this.h = h - 1;
+			} else {
+				if (m >= 60) {
+					this.m = m - 60;
+					this.h = h + 1;
+				} else {
+					this.m = m;
 				}
-				else {
-			this.h = h;
-				}
-		if (m < 0) {
-			this.m = 60 + m;
-			this.h = h -1;
-			}	
-		else {
-			if (m >= 60) {
-				this.m = m - 60;
-				this.h = h + 1;
 			}
-			else {
-			this.m = m;
-				}
 		}
-			}	
 		s = 0;
-			
+
 	}
 
 	public ClockTime(int h, int m, int s) {
 		if (h < 0) {
 			this.h = 24 + h;
+		} else {
+			if (h >= 24) {
+				this.h = 0 + h - 24;
+			} else {
+				this.h = h;
 			}
-			else {
-				if (h >=24) {
-					this.h = 0 + (h - 24);
+			if (m < 0) {
+				this.m = 60 + m;
+				this.h = h - 1;
+			} else {
+				if (m >= 60) {
+					this.m = m - 60;
+					this.h = h + 1;
+				} else {
+					this.m = m;
 				}
-				else {
-			this.h = h;
-				}
-		if (m < 0) {
-			this.m = 60 + m;
-			this.h = h - 1;
-			}	
-		else {
-			if (m >= 60) {
-				this.m = m - 60;
-				this.h = h + 1;
 			}
-			else {
-			this.m = m;
-				}
-				}		
-			}	
+		}
 		if (s < 0) {
 			this.s = 60 + s;
 			this.m = m - 1;
-		}
-		else {
+		} else {
 			if (s >= 60) {
 				this.s = s - 60;
 				this.m = m + 1;
+			} else {
+				this.s = s;
 			}
-			else {
-			this.s = s;
-		}
 		}
 	}
 
 	public ClockTime(ClockTime ct) {
-		this.h = ct.h;
-		this.m = ct.m;
-		this.s = ct.s;
+		h = ct.h;
+		m = ct.m;
+		s = ct.s;
 	}
-	
-
 
 	public boolean same(ClockTime secondCt) {
-		if (this.getHours() == secondCt.getHours() && this.getMinutes() == secondCt.getMinutes() && this.getSeconds() == secondCt.getSeconds()) {
+		if (getHours() == secondCt.getHours() && getMinutes() == secondCt.getMinutes()
+				&& getSeconds() == secondCt.getSeconds()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
 
 	public void add(int x) {
 		int i = 0;
-		if (x>=60) {
-			while (x>=60) {
+		if (x >= 60) {
+			while (x >= 60) {
 				i = i + 1;
 				x = x - 60;
-				}
-			this.m = m + i;
+			}
+			m = m + i;
 		}
 	}
-				
-	public int diff(ClockTime secondCt){
+
+	public int diff(ClockTime secondCt) {
 		int hs;
 		int ms;
 		int dif;
-		
-		hs = (h * 60) * 60;
+
+		hs = h * 60 * 60;
 		ms = m * 60;
 		dif = hs + ms + s;
-	
+
 		return dif;
-		
+
 	}
 
 	public int getSeconds() {
 		if (s < 0) {
-			this.s = 60 + s;
-		}
-		else {
+			s = 60 + s;
+		} else {
 			if (s >= 60) {
-				this.s = s - 60;
+				s = s - 60;
 			}
-			
+
 		}
 		return s;
 	}
 
 	public int getMinutes() {
-		
+
 		if (m < 0) {
-			this.m = 60 + m;
-		}
-		else {
+			m = 60 + m;
+		} else {
 			if (m >= 60) {
-				this.m = m - 60;
-			}
-			else {
+				m = m - 60;
+			} else {
 				if (s < 0) {
-					this.s = 60 + s;
-					this.m = m - 1;
-				}
-				else {
+					s = 60 + s;
+					m = m - 1;
+				} else {
 					if (s >= 60) {
-						this.s = s - 60;
-						this.m = m + 1;
+						s = s - 60;
+						m = m + 1;
 					}
-					
+
 				}
 			}
 		}
 		return m;
-		
+
 	}
 
 	public int getHours() {
 		if (h < 0) {
-			this.h = 24 + h;
-		}
-		else {
+			h = 24 + h;
+		} else {
 			if (h >= 24) {
-				this.h = h - 24;
-			}
-			else {
+				h = h - 24;
+			} else {
 				if (m < 0) {
-					this.m = 60 + m;
-				}
-				else {
+					m = 60 + m;
+				} else {
 					if (m >= 60) {
-						this.m = m - 60;
-					}
-					else {
+						m = m - 60;
+					} else {
 						if (s < 0) {
-							this.s = 60 + s;
-							this.m = m - 1;
-						}
-						else {
+							s = 60 + s;
+							m = m - 1;
+						} else {
 							if (s >= 60) {
-								this.s = s - 60;
-								this.m = m + 1;
+								s = s - 60;
+								m = m + 1;
 							}
-							
+
 						}
 					}
 				}
@@ -213,6 +190,7 @@ public class ClockTime {
 		}
 		return h;
 	}
+
 	public void output() {
 		System.out.println(String.format("%s:%s:%s", h, m, s));
 	}
